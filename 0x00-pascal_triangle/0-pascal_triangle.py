9abf7f963a5573a07f2851faf6f3e9b84a9cb0d6
girmesh03 <girmachewzewdie03@gmail.com>
2023-07-27 17:43:53 +0300

    make 0-pascal_triangle executable

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,12 @@
+#!/usr/bin/python3
+
+""" Module that returns a list of lists of integers representing
+    the Pascal’s triangle of n """
+
+
 def pascal_triangle(n):
+    """ Function that returns a list of lists of integers representing
+        the Pascal’s triangle of n """
     if n <= 0:
         return []
 
