181aa3357146583ad9d3491d74c2f2f7d075fcb5
girmesh03 <girmachewzewdie03@gmail.com>
2023-08-09 01:45:58 +0300

    0-main.py added and 0-minoperations.py updated

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -18,22 +18,6 @@ H => Copy All => Paste => HH => Paste =>HHH => Copy All => Paste => HHHHHH =>
 Paste => HHHHHHHHH
 
 Number of operations: 6
-
-Eg. without Recursion:
-
-def minOperations(n):
-    if n <= 1:
-        return 0
-    operations = 0
-    while n > 1:
-        if n % 2 == 0:
-            n = n // 2
-            operations += 2
-        else:
-            n = n - 1
-            operations += 1
-    return operations
-
 """
 
 
@@ -43,7 +27,17 @@ def minOperations(n):
 
     if n <= 1:
         return 0
-    if n % 2 == 0:
-        return 2 + minOperations(n // 2)
-    else:
-        return 1 + minOperations(n - 1)
+
+    operations = n  # Initialize with a worst-case value
+
+    # Try all possible divisors up to the square root of n
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            # We can generate n by copying i times and
+            # then pasting n // i times
+            operations = min(operations, minOperations(i) + n // i)
+            # We can generate n by copying n // i times
+            # and then pasting i times
+            operations = min(operations, minOperations(n // i) + i)
+
+    return operations
