6aa09680a15009ca709abaf8f0054183119a6332
girmesh03 <girmachewzewdie03@gmail.com>
2023-08-16 03:26:28 +0300

    0-stats.py added

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import sys
+
+def print_stats(total_size, status_codes):
+    print("File size: {}".format(total_size))
+    for code in sorted(status_codes):
+        print("{}: {}".format(code, status_codes[code]))
+
+
+if __name__ == "__main__":
+    total_size = 0
+    status_codes = {}
+    i = 0
+    try:
+        for line in sys.stdin:
+            i += 1
+            line = line.split()
+            total_size += int(line[-1])
+            code = line[-2]
+            if code in status_codes:
+                status_codes[code] += 1
+            else:
+                status_codes[code] = 1
+            if i % 10 == 0:
+                print_stats(total_size, status_codes)
+        print_stats(total_size, status_codes)
+    except KeyboardInterrupt:
+        print_stats(total_size, status_codes)
+        raise
