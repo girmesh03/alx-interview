4d220d4c285e9ce78bf39f5b7f49e79fe5e23667
girmesh03 <girmachewzewdie03@gmail.com>
2023-08-31 09:27:50 +0300

    0-nqueens.py updated #3

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,47 +1,52 @@
 #!/usr/bin/python3
-"""N Queens"""
-import sys
+"""
+Module for 0x0C. N Queens.
+"""
+from sys import argv, exit
 
-def print_board(board):
-    """Print allocated positions of the queens"""
-    b = [[i, board[i]] for i in range(len(board))]
-    print(b)
 
-def is_position_safe(board, row, col):
-    """Checks if the position is safe for the queen"""
-    for prev_row in range(row):
-        if board[prev_row] == col or \
-           abs(board[prev_row] - col) == abs(prev_row - row):
-            return False
-    return True
+def solveNQueens(n):
+    """Program that solves the N queens problem"""
+    res = []
+    queens = [-1] * n
+    col_diff = [0] * n
+    diag_diff = [0] * (2 * n)
+    diag_sum = [0] * (2 * n)
 
-def safe_positions(board, row, n):
-    """Find all safe positions where the queen can be allocated"""
-    if row == n:
-        print_board(board)
+    def dfs(index):
+        """Recursively resolves the N queens problem"""
+        if index == len(queens):  # n queens have been placed correctly
+            res.append(queens[:])
+            return  # backtracking
+        for i in range(len(queens)):
+            if not col_diff[i] and not diag_diff[index - i] and not diag_sum[index + i]:
+                queens[index] = i
+                col_diff[i] = 1
+                diag_diff[index - i] = 1
+                diag_sum[index + i] = 1
+                dfs(index + 1)
+                col_diff[i] = 0
+                diag_diff[index - i] = 0
+                diag_sum[index + i] = 0
+
+    dfs(0)
+    return [[[row, col] for row, col in enumerate(queens)] for queens in res]
+
+
+if __name__ == "__main__":
+    if len(argv) < 2:
+        print('Usage: nqueens N')
+        exit(1)
+    try:
+        n = int(argv[1])
+    except ValueError:
+        print('N must be a number')
+        exit(1)
+
+    if n < 4:
+        print('N must be at least 4')
+        exit(1)
     else:
-        for col in range(n):
-            if is_position_safe(board, row, col):
-                board[row] = col
-                safe_positions(board, row + 1, n)
-
-def create_board(size):
-    """Generates the board"""
-    return [0] * size
-
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    exit(1)
-
-try:
-    n = int(sys.argv[1])
-except ValueError:
-    print("N must be a number")
-    exit(1)
-
-if n < 4:
-    print("N must be at least 4")
-    exit(1)
-
-board = create_board(n)
-safe_positions(board, 0, n)
+        result = solveNQueens(n)
+        for board in result:
+            print(board)
