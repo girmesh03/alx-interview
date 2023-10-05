bc3833e416a832217c9f6e98de945f0941331691
girmesh03 <girmachewzewdie03@gmail.com>
2023-10-05 20:25:43 +0300

    0-prime_game.py updated #1

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -9,14 +9,20 @@ def isWinner(x, nums):
         return None
     n = max(nums)
     sieve = [True] * (n + 1)
-    sieve[0] = sieve[1] = False
+    sieve[0] = False
+    sieve[1] = False
     for i in range(2, int(n ** 0.5) + 1):
         if sieve[i]:
             for j in range(i * i, n + 1, i):
                 sieve[j] = False
-    sieve = [i for i, prime in enumerate(sieve) if prime]
-    c = 0
+    sieve2 = sieve[:]
+    for i in range(1, n + 1):
+        sieve2[i] = sieve2[i - 1] + (sieve[i])
+    player = 0
     for n in nums:
-        if n in sieve:
-            c += 1
-    return "Maria" if c % 2 else "Ben"
+        player += sieve2[n] % 2 == 1
+    if player * 2 == len(nums):
+        return None
+    if player * 2 > len(nums):
+        return "Maria"
+    return "Ben"
